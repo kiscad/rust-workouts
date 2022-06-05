42b4d35fa23e4b85ab8b5fe18edbaf50c98d113f
mojave2 <0109chenchen@gmail.com>
2022-06-05 17:19:25 +0800

    test commit on laptop

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, Rust!");
+    println!("GCD of {} and {} is {}",
+             43,
+             44,
+             gcd(44, 43));
 }
+
+fn gcd(n: u32, m: u32) -> u32 {
+    fn iter(small: u32, large: u32) -> u32 {
+        match small {
+            0 => large,
+            x => iter(large % x, x),
+        }
+    }
+    match n < m {
+        true => iter(n, m),
+        false => iter(m, n),
+    }
+}
+
