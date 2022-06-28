8e2b632575729297888de23d043f456c2f592eca
mojave2 <chenchen145@huawei.com>
2022-06-28 15:20:41 +0800

    update

--- scrape_url/src/main.rs
+++ scrape_url/src/main.rs
@@ -1,11 +1,24 @@
+use std::env;
 use std::fs;
 
 fn main() {
-    let url = "https://www.rust-lang.org/";
-    let output = "rust.md";
+    // let url = "https://www.rust-lang.org/";
+    // let output = "rust.md";
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    if args.len() < 2 {
+        eprintln!("Not enough args provide");
+        eprintln!("Usage: cargo run -- URL-ADDR MD-FILE-PATH");
+        std::process::exit(1);
+    }
+    let (url, output) = (&args[0], &args[1]);
+    println!("url: {}", url);
+    println!("output file: {}", output);
 
     println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let body = reqwest::blocking::get(url)
+        .expect("Invalid url")
+        .text()
+        .unwrap();
 
     println!("Converting html to markdown...");
     let md = html2md::parse_html(&body);
